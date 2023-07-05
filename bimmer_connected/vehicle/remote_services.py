5b33b1cc919f16a690449ba0e2a14a3f3101032f
tripple-jjj <64060616+tripple-jjj@users.noreply.github.com>
2023-07-05 14:56:46 +0200

    Add files via upload
    Added logging of vehicle specific JSON message content

--- bimmer_connected/vehicle/remote_services.py
+++ bimmer_connected/vehicle/remote_services.py
@@ -16,7 +16,7 @@ from bimmer_connected.const import (
     VEHICLE_CHARGING_START_STOP_URL,
     VEHICLE_POI_URL,
 )
-from bimmer_connected.models import ChargingSettings, MyBMWRemoteServiceError, PointOfInterest, StrEnum
+from bimmer_connected.models import ChargingSettings, PointOfInterest, StrEnum
 from bimmer_connected.utils import MyBMWJSONEncoder
 from bimmer_connected.vehicle.charging_profile import (
     MAP_CHARGING_MODE_TO_REMOTE_SERVICE,
@@ -120,14 +120,12 @@ class RemoteServices:
                 params=params,
                 content=json.dumps(data, cls=MyBMWJSONEncoder) if data else None,
             )
-            event_id = response.json().get("eventId") if response.content else None
+        event_id = response.json().get("eventId") if response.content else None
 
-            # Get status via event_id or assume successful execution as HTTP errors would raise exceptions before
-            status = (
-                await self._block_until_done(client, event_id)
-                if event_id
-                else RemoteServiceStatus({"eventStatus": "EXECUTED"})
-            )
+        # Get status via event_id or assume successful execution as HTTP errors would raise exceptions before
+        status = (
+            await self._block_until_done(event_id) if event_id else RemoteServiceStatus({"eventStatus": "EXECUTED"})
+        )
 
         # If vehicle data needs to be refresh, wait 2 times polling cycle and refresh completely
         if refresh:
@@ -136,7 +134,7 @@ class RemoteServices:
 
         return status
 
-    async def _get_remote_service_status(self, client: MyBMWClient, event_id: str) -> RemoteServiceStatus:
+    async def _get_remote_service_status(self, event_id: str) -> RemoteServiceStatus:
         """Return execution status of the last remote service that was triggered."""
 
         _LOGGER.debug("getting remote service status for '%s'", event_id)
@@ -145,7 +143,7 @@ class RemoteServices:
             response = await client.post(url)
         return RemoteServiceStatus(response.json(), event_id=event_id)
 
-    async def _block_until_done(self, client: MyBMWClient, event_id: str) -> RemoteServiceStatus:
+    async def _block_until_done(self, event_id: str) -> RemoteServiceStatus:
         """Keep polling the server until we get a final answer.
 
         :raises TimeoutError: if there is no final answer before _POLLING_TIMEOUT
@@ -154,15 +152,13 @@ class RemoteServices:
         fail_after = datetime.datetime.now() + datetime.timedelta(seconds=_POLLING_TIMEOUT)
         while datetime.datetime.now() < fail_after:
             await asyncio.sleep(_POLLING_CYCLE)
-            status = await self._get_remote_service_status(client, event_id)
+            status = await self._get_remote_service_status(event_id)
             _LOGGER.debug("current state of '%s' is: %s", event_id, status.state.value)
             if status.state == ExecutionState.ERROR:
-                raise MyBMWRemoteServiceError(
-                    f"Remote service failed with state '{status.state}'. Response: {status.details}"
-                )
+                raise Exception(f"Remote service failed with state '{status.state}'. Response: {status.details}")
             if status.state not in [ExecutionState.UNKNOWN, ExecutionState.PENDING, ExecutionState.DELIVERED]:
                 return status
-        raise MyBMWRemoteServiceError(
+        raise TimeoutError(
             f"Did not receive remote service result for '{event_id}' in {_POLLING_TIMEOUT} seconds. "
             f"Current state: {status.state.value}"
         )
@@ -268,6 +264,7 @@ class RemoteServices:
             raise ValueError("Vehicle does not support setting charging profile.")
 
         target_charging_profile = self._vehicle.charging_profile.format_for_remote_service()
+        
 
         if charging_mode and not charging_mode == ChargingMode.UNKNOWN:
             target_charging_profile["chargingMode"]["type"] = MAP_CHARGING_MODE_TO_REMOTE_SERVICE[charging_mode]
@@ -277,7 +274,9 @@ class RemoteServices:
 
         if precondition_climate:
             target_charging_profile["isPreconditionForDepartureActive"] = precondition_climate
-
+            """debug JSON data added by jjj on 22.3.2023"""   
+        
+        _LOGGER.debug("Charging Profile update - targetChargingProfile: '%s' ", target_charging_profile)
         return await self.trigger_remote_service(
             Services.CHARGING_PROFILE,
             data=target_charging_profile,
